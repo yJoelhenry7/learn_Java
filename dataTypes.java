da00fbb325c85dcee65ffbddedcc8d8e905155dd
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-09-12 00:05:51 +0530

    adding dataType.java

---
+++ dataTypes.java
@@ -0,0 +1,23 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5856l;
+
+        float f= 5.8f;
+        double d = 5.8;
+
+        char c = 'k';
+        boolean b = true;
+
+        System.out.println("int : "+num1);
+        System.out.println("byte : "+by);
+        System.out.println("short : "+sh);
+        System.out.println("long : "+l);
+        System.out.println("float : "+f);
+        System.out.println("char : "+c);
+        System.out.println("boolean : "+b);
+
+    }
+}
