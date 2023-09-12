86dfc36613bfd521a712080e271a7d8e08fa5e08
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-09-12 20:29:48 +0530

    add relational.java

---
+++ relational.java
@@ -0,0 +1,20 @@
+public class relational {
+    public static void main(String[] args) {
+        int x =6;
+        int y =7;
+
+        boolean result = x < y;
+        boolean result1 = x > y;
+        boolean result2 = x <= y;
+        boolean result3 = x >= y;
+        boolean result4 = x != y;
+        boolean result5 = x == y;
+
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+    }
+}
