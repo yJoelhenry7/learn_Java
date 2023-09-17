b5ce3267ec7470f75a32f9f46e197791412aad39
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-09-17 19:33:38 +0530

    adding while loop

---
+++ whileLoop.java
@@ -0,0 +1,15 @@
+public class whileLoop{
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<4)
+        {
+            System.out.println("Hi" + i);
+            int j =1;
+            while(j<=3){
+                System.out.println("hello" + j);
+                j++;
+            }
+            i++;
+        }
+    }
+}
